3844b65288bad4fa6f83633ea971c427ba176bc5
ishriom53tyagi <17bcs053@smvdu.ac.in>
2021-10-23 17:22:53 +0530

    changes has been done for upload

--- utils/checkClient.js
+++ utils/checkClient.js
@@ -3,10 +3,12 @@ const getDb = require('./database').getDb
 module.exports = async function (req, res, next) {
     try {
         let db = getDb();
+        console.log("req query path value is here", req.query.path);
         if (!req.query.path) {
             return res.status(403).send({ status: "403", message: "Access Denied" });
         }
         let merchantIp = await db.collection("merchantIp").findOne({ merchantIp: req.query.path });
+        console.log("merchant IP value is here", merchantIp);
         if (!merchantIp) {
             return res.status(403).send({ status: "403", message: "Access Denied" });
         }
